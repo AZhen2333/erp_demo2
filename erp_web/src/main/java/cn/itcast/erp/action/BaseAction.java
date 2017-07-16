cf88d55ef0521a38fdc55d1ea8146060878dfb17
AZhen <1294996832@qq.com>
2017-07-16 16:26:59 +0800

    day03-02:建立dep和emp的联系，避免json的循环引导符，页面显示部门名字

--- erp_web/src/main/java/cn/itcast/erp/action/BaseAction.java
+++ erp_web/src/main/java/cn/itcast/erp/action/BaseAction.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.struts2.ServletActionContext;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import cn.itcast.erp.biz.IBaseBiz;
 
@@ -85,7 +86,7 @@ public class BaseAction<T> {
 		mapData.put("total", total);
 		mapData.put("rows", list);
 		//把部门列表转JSON字符串
-		String listString = JSON.toJSONString(mapData);
+		String listString = JSON.toJSONString(mapData,SerializerFeature.DisableCircularReferenceDetect);
 		write(listString);
 	}
 	
